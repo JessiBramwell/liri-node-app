d9792353853280c1b7c83a52ec5dce38386b9be4
jessiBramwell <bramibly@gmail.com>
2019-04-12 10:49:29 -0600

    refactering and added inquirer

--- liri.js
+++ liri.js
@@ -1,41 +1,47 @@
-var hr = '-------------------------------------------------------------------------------------------------------------------------------------- \n'
-require("dotenv").config();
-var fs = require("fs");
+require('dotenv').config();
+var fs = require('fs');
 var moment = require('moment');
-var keys = require("./keys.js");
-var axios = require("axios");
+var keys = require('./keys.js');
+var axios = require('axios');
+var inquirer = require('inquirer')
+var chalk = require('chalk');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-var nodeArgs = process.argv;
-var action = process.argv[2];
-var media = '';
+var output = {}
 
-for (let i = 3; i < nodeArgs.length; i++) {
-  if (i > 3 && nodeArgs.length) {
-    media = media + "+" + nodeArgs[i]
-  } else {
-    media += nodeArgs[i]
-  }
-}
-
-switch (action) {
-  case "concert-this":
-    concertQuery(media)
-    break;
-  case "spotify-this-song":
-    spotifyQuery(media)
-    break;
-  case "movie-this":
-    movieQuery(media)
-    break;
-  case "do-what-it-says":
-    readArg()
-    break;
-  default:
-    console.log("that is not an approved request");
-}
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to search for?',
+      choices: ['Concerts', 'A Song on Spotify', 'Movie Details'/*, 'Surpise me'*/],
+      name: 'action',
+    },
+    {
+      type: 'input',
+      message: 'Search:',
+      name: 'media'
+    }
+  ]).then(function (resp) {
+    if (resp) {
+      switch (resp.action) {
+        case 'Concerts':
+          concertQuery(resp.media)
+          break;
+        case 'A Song on Spotify':
+          spotifyQuery(resp.media)
+          break;
+        case 'Movie Details':
+          movieQuery(resp.media)
+          break;
+        default:
+          console.log("that is not an approved request");
+      }
+    }
+  });
 
 function movieQuery(media) {
+
   if (media === "") {
     media = "Mr. Nobody"
   }
@@ -43,39 +49,37 @@ function movieQuery(media) {
   axios.get(queryUrl).then(
     function (response) {
       var data = response.data;
-      var output = {
-        top: hr,
-        title: `${data.Title} (${data.Rated}) \n`,
-        plot: `Plot: ${data.Plot} \n`,
-        cast: `Cast: ${data.Actors} \n`,
-        meta: `Year: ${data.Year} | ${data.Country} | ${data.Language} | Ratings: imdb - ${data.imdbRating}, Rotten Tomatoes - ${data.Ratings[1].Value} \n`,
-        bottom: hr,
-      }
 
-      for (var prop in output) {
-        console.log(output[prop])
-        append(output[prop])
+      if (!Array.isArray(data) || !data.length) {
+        console.log(chalk.red(`There are no movies showing for: ${capitalize(media)}`))
+      } else {
+        output = {
+          title: `${data.Title} (${data.Rated}) \n`,
+          plot: `Plot: ${data.Plot} \n`,
+          cast: `Cast: ${data.Actors} \n`,
+          meta: `Year: ${data.Year} | ${data.Country} | ${data.Language} | Ratings: imdb - ${data.imdbRating}, Rotten Tomatoes - ${data.Ratings[1].Value} \n`,
+        }
+        append(output)
       }
-    }
-  );
+    });
 }
 
 function concertQuery(media) {
+
   var queryUrl = `https://rest.bandsintown.com/artists/${media}/events?app_id=codingbootcamp`
   axios.get(queryUrl).then(
     function (response) {
+      var location, result
       var data = response.data
-      var lineup = []
-      var output = {}
-      var location
 
       if (!Array.isArray(data) || !data.length) {
-        console.log(`There are no concerts showing for ${capitalize(media)}`)
-        console.log("--")
+        console.log(chalk.red(`There are no concerts showing for: ${capitalize(media)}`))
+
       } else {
-        printSearch(media)
+        capitalize(media)
+        output.result = []
         for (let i = 0; i < 5; i++) {
-          lineup = data[i].lineup[0]
+          output.name = data[i].lineup[0]
 
           if (!data[i].venue.region) {
             location = data[i].venue.country
@@ -83,71 +87,88 @@ function concertQuery(media) {
             location = data[i].venue.region
           }
 
-          output = {
-            event: `${lineup} @ ${data[i].venue.name} | ${data[i].venue.city}, ${location} | ${moment(data[i].datetime).format("dddd, MMMM Do, h:mm a")} \n`,
-          }
+          result = `@ ${data[i].venue.name} | ${data[i].venue.city}, ${location} | ${moment(data[i].datetime).format("dddd, MMMM Do, h:mm a")} \n`
 
-          for (var prop in output) {
-            console.log(output[prop])
-            // append(output[prop])
-          }
+          output.result.push(result)
         }
-        console.log(hr)
-        append(hr)
+        append(output)
       }
     }
-  );
+  )
 }
 
 function spotifyQuery(media) {
+
   if (!media) {
     media = "The Sign, Ace of Base"
   }
-  spotify.search({ type: 'track', query: media, }, function (err, data) {
-    var data = data.tracks.items;
+  spotify.search({ type: 'track', query: media, limit: 5 }, function (err, response) {
+    var data, result
 
     if (err) {
       return console.log('Error occurred: ' + err);
     } else {
-      printSearch(media)
+      output.name = capitalize(media)
+      output.result = []
       for (let i = 0; i < 5; i++) {
-        var output = {
-          track: `${data[i].name} | ${data[i].album.artists[0].name}, ${data[i].album.name} | ${data[i].external_urls.spotify} \n`,
-        }
+        data = response.tracks.items[i];
 
-        for (var prop in output) {
-          console.log(output[prop])
-          append(output[prop])
-        }
+        result = `${data.name} | ${data.album.artists[0].name}, ${data.album.name} | ${data.external_urls.spotify} \n`
+        output.result.push(result)
       }
-      console.log(hr)
-      append(hr)
+      append(output)
     }
   });
 }
 
-function readArg() {
+function readQuery() {
   fs.readFile("random.txt", 'utf8', function (err, data) {
-    if(err) {
+    if (err) {
       return console.log(error);
     } else {
       console.log(data)
-      process.argv[2] = data
+      data = data.replace(/['"]+/g, '')
+      var readArray = data.split(',')
+      console.log(readArray)
+      action = readArray[0]
+      media = readArray[1]
+      return media
     }
-  })
+  });
 }
 
 function append(output) {
-  fs.appendFile("log.txt", output, function (err) {
+  var entry
+  var str = '\n'
+  var timeStamp = moment().format('dddd, MMMM Do, h:mm a') + '\n';
+  var hr = '======================================================================== \n';
+
+  if (output.name && output.result) {
+    str = '\n' + output.name + '\n'
+
+    output.result.forEach(el => {
+      str += el
+    });
+  } else {
+    for (var prop in output) {
+      str += output[prop]
+    }
+  }
+
+  entry = hr.concat(str, hr)
+  console.log(chalk.green(entry));
+
+  fs.appendFile("log.txt", timeStamp.concat(entry), function (err) {
     if (err) {
       console.log(err);
     }
-  })
+  });
 }
 
-function printSearch(str) {
-  if (str.includes("+")) {
-    str = str.split("+");
+
+function capitalize(str) {
+  if (str.includes(" ")) {
+    str = str.split(" ");
     for (var i = 0; i < str.length; i++) {
       str[i] = str[i][0].toUpperCase() + str[i].slice(1);
     }
@@ -155,8 +176,7 @@ function printSearch(str) {
   } else {
     str = str[0].toUpperCase() + str.slice(1)
   }
-  console.log(`Search results for: ${str} \n`)
-  append(hr)
+  console.log(chalk.bold(`Search results for: ${chalk.cyan(str)} \n`))
   return str
 }
 // .`node liri.js do-what-it-says`
